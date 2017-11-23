b9fa3d3c750856d7d1c61c146ed64d434db027ee
Tyler Duffus <tyled94@hotmail.com>
2017-11-23 22:15:19 +1000

    Added: Tilemap class which supports proper tiling, layers and levels.

--- Habitat/MapEditor/Program.cs
+++ Habitat/MapEditor/Program.cs
@@ -72,73 +72,43 @@ namespace Habitat.MapEditor
             };
             #endregion
 
-            var texture = new Texture("floors.png");
-
-            const int spriteSize = 32;
-            var sourceCols = texture.Size.X / spriteSize;
-
             #region map setup
             var rnd = new Random();
-            //var mapSize = new Vector2u((uint)rnd.Next(1, 1024), (uint)rnd.Next(1, 1024));
-            var mapSize = new Vector2u(1024, 1024);
-            Console.WriteLine("Create map of size {0}x{1} ({2} tiles)", mapSize.X, mapSize.Y, mapSize.X * mapSize.Y);
-            var map = new uint[mapSize.X * mapSize.Y];
 
-            for (var i = 0; i < map.Length; ++i)
+            var TileMap = new TileMap();
+            TileMap.MapSize = new Vector2u((uint)rnd.Next(1, 1024), (uint)rnd.Next(1, 1024));
+            Console.WriteLine("Created map of size: {0}x{1} ({2} tiles)", TileMap.MapSize.X, TileMap.MapSize.Y, TileMap.MapSize.X * TileMap.MapSize.Y);
+            TileMap.Textures.Add(new TileMap.TextureMeta("space.png"));
+            TileMap.Textures.Add(new TileMap.TextureMeta("floors.png"));
+
+            var firstLevel = new TileMap.Level();
+            for (uint i = 0; i < TileMap.MapSize.X * TileMap.MapSize.Y; ++i)
             {
-                map[i] = (uint)(rnd.Next(2, 944));
+                var spaceLayer = new TileMap.Level.Tile.Layer
+                {
+                    SpriteSheetIndex = 0,
+                    SpriteIndex = (uint)rnd.Next(0, 5)
+                };
+
+                var tile = new TileMap.Level.Tile();
+                tile.Layers.Add(spaceLayer);
+
+                if (rnd.Next(0, 100) > 80)
+                {
+                    var newLayer = new TileMap.Level.Tile.Layer
+                    {
+                        SpriteSheetIndex = 1,
+                        SpriteIndex = (uint)rnd.Next(2, 945)
+                    };
+
+                    tile.Layers.Add(newLayer);
+                }
+
+                firstLevel.Tiles.Add(tile);
             }
-            #endregion
 
-            #region map population
-            var renderStates = RenderStates.Default;
-            renderStates.Texture = texture;
-            var vertexArray = new VertexArray(PrimitiveType.Quads);
-
-            for (var index = 0; index < map.Length; ++index)
-            {
-                var textureIndex = map[index];
-
-                // sprite rect in source
-                int sourceX = (int)(textureIndex % sourceCols) * spriteSize;
-                int sourceY = (int)(textureIndex / sourceCols) * spriteSize;
-
-                // position on map
-                int mapX = (int)(index % mapSize.X) * spriteSize;
-                int mapY = (int)(index / mapSize.X) * spriteSize;
-
-                // Top left
-                vertexArray.Append(new Vertex()
-                {
-                    Position = new Vector2f(mapX, mapY),
-                    TexCoords = new Vector2f(sourceX, sourceY),
-                    Color = Color.White
-                });
-
-                // Top right
-                vertexArray.Append(new Vertex()
-                {
-                    Position = new Vector2f(mapX + spriteSize, mapY),
-                    TexCoords = new Vector2f(sourceX + spriteSize, sourceY),
-                    Color = Color.White
-                });
-
-                // Bottom right
-                vertexArray.Append(new Vertex()
-                {
-                    Position = new Vector2f(mapX + spriteSize, mapY + spriteSize),
-                    TexCoords = new Vector2f(sourceX + spriteSize, sourceY + spriteSize),
-                    Color = Color.White
-                });
-
-                // Bottom left
-                vertexArray.Append(new Vertex()
-                {
-                    Position = new Vector2f(mapX, mapY + spriteSize),
-                    TexCoords = new Vector2f(sourceX, sourceY + spriteSize),
-                    Color = Color.White
-                });
-            }
+            TileMap.Levels.Add(firstLevel);
+            TileMap.Rebuild(); // Rebuild after modification
             #endregion
 
             while (window.IsOpen)
@@ -147,7 +117,7 @@ namespace Habitat.MapEditor
                 window.Clear();
 
                 // draw tilemap
-                window.Draw(vertexArray, renderStates);
+                window.Draw(TileMap);
 
                 /*
                 // draw grid
---
+++ Habitat/MapEditor/TileMap.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections.Generic;
+
+using SFML.Graphics;
+using SFML.System;
+
+namespace Habitat.MapEditor
+{
+    public class TileMap : Drawable
+    {
+        public class Level
+        {
+            public class Tile
+            {
+                public class Layer
+                {
+                    public uint SpriteSheetIndex
+                    {
+                        get;
+                        internal set;
+                    }
+
+                    public uint SpriteIndex
+                    {
+                        get;
+                        internal set;
+                    }
+                }
+
+                public List<Layer> Layers
+                {
+                    get;
+                    internal set;
+                }
+
+                public Tile()
+                {
+                    Layers = new List<Layer>();
+                }
+            }
+
+            public List<Tile> Tiles
+            {
+                get;
+                internal set;
+            }
+
+            public Level()
+            {
+                Tiles = new List<Tile>();
+            }
+        }
+
+        public List<Level> Levels
+        {
+            get;
+            internal set;
+        }
+
+        public const uint SpriteSize = 32;
+        public static Vector2u MapSize
+        {
+            get;
+            internal set;
+        }
+
+        public class TextureMeta
+        {
+            public uint Columns
+            {
+                get;
+                internal set;
+            }
+
+            public uint Rows
+            {
+                get;
+                internal set;
+            }
+
+            public Texture Texture
+            {
+                get;
+                internal set;
+            }
+
+            public RenderStates RenderStates
+            {
+                get;
+                internal set;
+            }
+
+            public TextureMeta(string filename)
+            {
+                Texture = new Texture(filename);
+                Columns = Texture.Size.X / SpriteSize;
+                Rows    = Texture.Size.Y / SpriteSize;
+
+                RenderStates = new RenderStates(BlendMode.Alpha, Transform.Identity, Texture, null);
+            }
+
+            public VertexArray GetSprite(int tileIndex, uint spriteIndex)
+            {
+                var sourcePos = new Vector2u(
+                    (spriteIndex % Columns) * SpriteSize,
+                    (spriteIndex / Columns) * SpriteSize
+                );
+
+                var mapPos = new Vector2i(
+                    (int)((tileIndex % MapSize.X) * SpriteSize),
+                    (int)((tileIndex / MapSize.X) * SpriteSize)
+                );
+
+                var vertexArray = new VertexArray(PrimitiveType.Points, 4);
+
+                // Top Left
+                vertexArray[0] = new Vertex()
+                {
+                    Position  = (Vector2f)mapPos,
+                    Color     = Color.White,
+                    TexCoords = (Vector2f)sourcePos
+                };
+
+                // Top Right
+                vertexArray[1] = new Vertex()
+                {
+                    Position  = (Vector2f)(mapPos + new Vector2i((int)SpriteSize, 0)),
+                    Color     = Color.White,
+                    TexCoords = (Vector2f)(sourcePos + new Vector2u(SpriteSize, 0))
+                };
+
+                // Bottom Right
+                vertexArray[2] = new Vertex()
+                {
+                    Position = (Vector2f)(mapPos + new Vector2i((int)SpriteSize, (int)SpriteSize)),
+                    Color = Color.White,
+                    TexCoords = (Vector2f)(sourcePos + new Vector2u(SpriteSize, SpriteSize))
+                };
+                
+                // Bottom Left
+                vertexArray[3] = new Vertex()
+                {
+                    Position = (Vector2f)(mapPos + new Vector2i(0, (int)SpriteSize)),
+                    Color = Color.White,
+                    TexCoords = (Vector2f)(sourcePos + new Vector2u(0, SpriteSize))
+                };
+
+                return vertexArray;
+            }
+        }
+
+        public List<TextureMeta> Textures
+        {
+            get;
+            internal set;
+        }
+
+        private Dictionary<uint, VertexArray> textureDictionary = new Dictionary<uint, VertexArray>();
+
+        public TileMap()
+        {
+            Levels = new List<Level>();
+            Textures = new List<TextureMeta>();
+        }
+
+        public void Rebuild()
+        {
+            textureDictionary.Clear();
+
+            foreach(var level in Levels)
+            {
+                for (var tileIndex = 0; tileIndex < level.Tiles.Count; ++tileIndex)
+                {
+                    var tile = level.Tiles[tileIndex];
+                    var topLayer = tile.Layers[tile.Layers.Count - 1];
+
+                    var spriteVertexArray = Textures[(int)topLayer.SpriteSheetIndex].GetSprite(tileIndex, topLayer.SpriteIndex);
+
+                    if (textureDictionary.ContainsKey(topLayer.SpriteSheetIndex) == false)
+                        textureDictionary[topLayer.SpriteSheetIndex] = new VertexArray(PrimitiveType.Quads);
+
+                    textureDictionary[topLayer.SpriteSheetIndex].Append(spriteVertexArray[0]);
+                    textureDictionary[topLayer.SpriteSheetIndex].Append(spriteVertexArray[1]);
+                    textureDictionary[topLayer.SpriteSheetIndex].Append(spriteVertexArray[2]);
+                    textureDictionary[topLayer.SpriteSheetIndex].Append(spriteVertexArray[3]);
+                }
+            }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            foreach(var entry in textureDictionary)
+            {
+                target.Draw(entry.Value, Textures[(int)entry.Key].RenderStates);
+            }
+        }
+    }
+}
